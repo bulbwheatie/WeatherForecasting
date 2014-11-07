5c4a2c4c077f442e4abb95848c83bc9146fdd204
Andrew Pillsbury <andrew.d.pillsbury@gmail.com>
2014-11-06 23:50:01 -0500

    we did a thing. it doesn't work. sad face tears

---
+++ src/RNN/checker.m
@@ -0,0 +1,43 @@
+
+function checker(data, std_mean) 
+    [Winput, Wprev, Woutput, error] = train(data);
+    i = 1;
+    X = data(i:i+11,:);
+    values_pred = zeros(6,6);
+    values_actual = data(i+12:i+17,:);
+    
+    for j=1:6
+        [temp_y, signals] = feedForward([ones(size(X,1), 1) X], Winput, Wprev, Woutput);
+        values_pred(j,:) = (temp_y(size(temp_y,1),:) .* std_mean(1,:)) + std_mean(2,:);
+        X = [X(2:size(X,1),:) ; values_pred(j,:)];
+    end
+    
+    x_axis = 1:size(values_pred, 1);
+    
+    %temperature
+    plot(x_axis, transpose(values_pred(:,2)), x_axis, transpose(values_actual(:,2)));
+    legend('y = Predicted temperature','y = Actual temperature','Location','southeast');
+    saveas(gcf, 'temperature.fig');
+    
+    %dewpoint
+    plot(x_axis, transpose(values_pred(:,3)), x_axis, transpose(values_actual(:,3)));
+    legend('y = Predicted dewpoint','y = Actual dewpoint','Location','southeast');
+    saveas(gcf, 'dewpoint.fig');
+    
+    %windspeed
+    plot(x_axis, transpose(values_pred(:,4)), x_axis, transpose(values_actual(:,4)));
+    legend('y = Predicted windspeed','y = Actual windspeed','Location','southeast');
+    saveas(gcf, 'windspeed.fig');
+    
+    %pressure
+    plot(x_axis, transpose(values_pred(:,6)), x_axis, transpose(values_actual(:,6)));
+    legend('y = Predicted windspeed','y = Actual windspeed','Location','southeast');
+    saveas(gcf, 'pressure.fig');
+    
+    %error
+    plot(1:size(error, 1), transpose(error));
+    legend('y = Squared error','Location','southeast');
+    saveas(gcf, 'error.fig');
+    
+    save('post_train.mat', 'Winput', 'Wprev', 'Woutput', 'data', 'std_mean', 'error');
+end
--- src/RNN/train.m
+++ src/RNN/train.m
@@ -1,5 +1,5 @@
 % TODO - erroar
-function [Winput, Wprev, Woutput] = train(X)
+function [Winput, Wprev, Woutput, error] = train(X)
     Y = X(2:size(X), :);
     X = X(1:size(X)-1, :);
     num_neurons  = 10;
@@ -9,17 +9,18 @@ function [Winput, Wprev, Woutput] = train(X)
     Wprev = initWeights(num_neurons, num_neurons,-1/10, 1/10);
     Woutput = initWeights(num_neurons, size(Y,2), -1/10, 1/10);
     
-    iter = 0;
-    max_iters = 1000;
-    lambda = 0.0000000001;
-    while (iter < max_iters)
-        i = mod(iter, size(X, 1) - 6) + 1;
+    iter = 1;
+    max_iters = 2000;
+    lambda = 0.000000000000000001;
+    error = zeros(max_iters, 1);
+    while (iter <= max_iters)
+        i = mod(iter, size(X, 1) - 12) + 1;
         
         %Forward pass through the network with a sequence of training data
-        [Ypred, signals] = feedForward(X(i:i+5,:), Winput, Wprev, Woutput);
-        
+        [Ypred, signals] = feedForward(X(i:i+11,:), Winput, Wprev, Woutput);
+        error(iter, 1) = sum((Ypred(size(Ypred,1),:) - Y(i+11,:)).^2);
         % Backpropagate and update weight matrices
-        [Winput, Wprev, Woutput] = backpropagate(X(i:i+5,:), Y(i:i+5,:), signals, Ypred, Winput, Wprev, Woutput, lambda);       
+        [Winput, Wprev, Woutput] = backpropagate(X(i:i+11,:), Y(i:i+11,:), signals, Ypred, Winput, Wprev, Woutput, lambda);       
         
         iter = iter + 1;
     end 
