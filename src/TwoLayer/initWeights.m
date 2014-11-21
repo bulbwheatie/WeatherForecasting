abfc8c7ceb543184bf19d25128a6ee92f3451a0a
bulbwheatie <bulbwheatie@gmail.com>
2014-11-21 13:28:09 -0500

    comments to checker small and lookahead

--- src/TwoLayer/initWeights.m
+++ src/TwoLayer/initWeights.m
@@ -1,3 +1,5 @@
+% Calculate weight of a specified size with random values in a specified
+% range
 function W = initWeights(row, col, min, max)
     W = (max-min).*rand(row, col) + min;
     %W(1,1) = 0;
