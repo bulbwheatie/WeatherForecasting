dfd321537aaa22f993c590c93b104280152257c7
bulbwheatie <bulbwheatie@gmail.com>
2014-11-20 22:43:22 -0500

    Adaptive training and final write-up

--- src/TwoLayer/checker_BP.m
+++ src/TwoLayer/checker_BP.m
@@ -1,56 +1,55 @@
 function error = checker_BP(data, std_mean)
     % Create a small data set and run the entire set through the training
     % process for each update and check that the error is indeed reducing.
-
-    batch_size = 11;
-    num_stacks = 12;
-    small_set = data(1:batch_size+num_stacks,2);
+    feature_num = 2; %Temperature
+    batch_size = 10;
+    num_stacks = 8;
+    batches = 50;
+    data.trainY = data.trainY(:,2, :);
+    data.validateY = data.validateY(:,2, :);
+    data.testY = data.testY(:, 2, :);
 
     %Random init of weights
-    num_neurons  = 20;
-    small_set = [small_set ones(size(small_set,1), 1)]; % Add bias feature
-    Winput_init = initWeights(2, num_neurons,-1/10, 1/10); % Create a d + 1 x n matrix for the extra bias feature
-    Winterior_init = initWeights(num_neurons + 1, num_neurons,-1/10, 1/10);
-    Wprev1_init = initWeights(num_neurons + 1, num_neurons,-1/10, 1/10);
-    Wprev2_init = initWeights(num_neurons + 1, num_neurons,-1/10, 1/10);
-    Woutput_init = initWeights(num_neurons + 1, 1, -1/2, 1/2); %Only single output feature in this case
-    
-    [Winput, Winterior, Wprev1, Wprev2, Woutput, error] = train_BP(small_set, Winput_init, Winterior_init, Wprev1_init, Wprev2_init, Woutput_init, 'temp', batch_size, num_stacks);
+    num_neurons  = 30;
+    Winput_init = initWeights(size(data.trainX,2), num_neurons,-1/10, 1/10); % Create a d + 1 x n matrix for the extra bias feature
+    Winterior_init = initWeights(num_neurons+1, num_neurons,-1/10, 1/10);
+    Wprev1_init = initWeights(num_neurons+1, num_neurons,-1/10, 1/10);
+    Wprev2_init = initWeights(num_neurons+1, num_neurons,-1/10, 1/10);
+    Woutput_init = initWeights(num_neurons+1, size(data.trainY, 2), -1/2, 1/2); %Only single output feature in this case
+   
+    [Winput, Winterior, Wprev1, Wprev2, Woutput, train_error, valid_error, test_error] = train_BP_adaptive(data, Winput_init, Winterior_init, Wprev1_init, Wprev2_init, Woutput_init, batch_size, batches);
     
     i = 1;
-    X = data(i:i+num_stacks-1,2);
-    X_naive = data(i:i+num_stacks-1,2);
+    X = data.testX(:, :, i:i+num_stacks-1);
     values_pred = zeros(6,1);
-    values_naive= zeros(6,1);
-    values_actual = data(i+num_stacks:i+num_stacks + 5,2);
+    values_actual = data.testY(:, :, i:i+num_stacks-1);
     
     for j=1:6
-        [temp_y, ~, ~,~, ~] = feedForward([X ones(size(X,1), 1)], Winput, Winterior, Wprev1, Wprev2, Woutput);
+        [temp_y, ~, ~,~, ~] = feedForward(X, Winput, Winterior, Wprev1, Wprev2, Woutput);
         values_pred(j,:) = (temp_y(j,1) .* std_mean(1,2)) + std_mean(2,2);
         values_actual(j,:) = (values_actual(j,1) .* std_mean(1,2)) + std_mean(2,2); %Restore the actual values too
-        %values_full = data(i+17+j,:);
-        %values_full(1,2) = values_pred(j,:);
-        X = [X(2:size(X,1)) ; temp_y(j,1)];
-
-        [naive_y, ~, ~] = feedForward([X_naive ones(size(X_naive,1), 1)], Winput_init, Winterior_init, Wprev1_init, Wprev2_init, Woutput_init);
-        values_naive(j,:) = (naive_y(j,1) .* std_mean(1,2)) + std_mean(2,2);
-        %naive_full = data(i+17+j,:);
-        %naive_full(1,2) = values_naive(j,:);
-        X_naive = [X_naive(2:size(X,1)) ; naive_y(j,1)];
+        values_full = data.trainX(:,:,i+num_stacks-1+j);
+        values_full(1,feature_num) = values_pred(j,:); 
+        X = [X(2:size(X,1)) ; values_full];
     end
     
     x_axis = (1:size(values_pred, 1) )* 4;
     
     %temperature -- comapre naive to trained
-    plot(x_axis, transpose(values_pred(:,1)), x_axis, transpose(values_naive(:,1)), x_axis, transpose(values_actual(:,1)));
+    plot(x_axis, transpose(values_pred(:,1)), x_axis, transpose(values_actual(:,1)));
     legend('y = Predicted temperature', 'y = Naive temperature', 'y = Actual temperature','Location','southoutside');
+    xlabel('Time (hours)');
+    ylabel('Temperature(C)');
     saveas(gcf, 'graphs/temperature2L.fig');
     
     %error
-    plot(1:size(error, 1), transpose(error));
-    legend('y = Squared error','Location','southeast');
+    plot(1:size(train_error, 1), transpose(train_error), 1:size(valid_error,1), transpose(valid_error));
+    legend('y = Squared train error', 'Y = Squared valid error', 'Location','southeast');
+    xlabel('Iterations');
+    ylabel('Squared Error');
+    title('Training and Validation Error for Batch Train');
     saveas(gcf, 'graphs/train_error2L.fig');
     
-    save('checker_BP.mat', 'Winput', 'Wprev1', 'Wprev2', 'Winterior', 'Woutput', 'data', 'std_mean', 'error');
+    save('checker_BP.mat', 'Winput', 'Wprev1', 'Wprev2', 'Winterior', 'Woutput', 'data', 'std_mean', 'train_error', 'valid_error', 'test_error');
     
 end
--- src/TwoLayer/train_BP_adaptive.m
+++ src/TwoLayer/train_BP_adaptive.m
@@ -1,14 +1,18 @@
 % Trains for a specified output feature
-function [Winput, Winterior, Wprev1, Wprev2, Woutput, error] = train_BP_adaptive(data, Winput, Winterior, Wprev1, Wprev2, Woutput, max_epochs)
+function [Winput_min, Winterior_min, Wprev1_min, Wprev2_min, Woutput_min, train_error, valid_error, test_error] = train_BP_adaptive(data, Winput, Winterior, Wprev1, Wprev2, Woutput, batch_size, batches)
 
-    error = zeros(max_epochs, 1);
-    lambda_init = 0.01;
-    num_samples = size(data.trainX, 3);
-    %Forward pass through the network with a sequence of training data
-    [Ypred, signals1, signals1prev, signals2, signals2prev] = feedForward_struct(data.trainX, Winput, Winterior, Wprev1, Wprev2, Woutput);
-    error(1,1) = sum(sum((Ypred(size(data.trainY, 1),:,:) - data.trainY(size(data.trainY, 1),:,:)).^2))/num_samples;
+    iter = 1;
+    lambda_init = 0.5;
+    train_error = zeros(batches, 1);
+    valid_error = zeros(batches, 1); 
+    diff = 1000;
+    valid_sum = 0;
+    lookback = 3;
+    tmp_error = 0;
+    i=1;
+    min_error = inf;
     
-    for epoch=1:max_epochs
+    while (iter <= batches && diff > 10^-5)
         lambda = lambda_init;
         Uinput = zeros(size(Winput));
         Uinterior = zeros(size(Winterior));
@@ -17,28 +21,43 @@ function [Winput, Winterior, Wprev1, Wprev2, Woutput, error] = train_BP_adaptive
         Uoutput = zeros(size(Woutput));
         
         %calculate update weights
-        for row=1:num_samples
-            [DjN, DiN, DpN] = backpropagate(data.trainX(:,:,row), data.trainY(:,:,row), signals1 + signals1prev, signals2 + signals2prev, Ypred(:,:,row), Winterior, Wprev1, Wprev2, Woutput);       
-            [Uinput, Uinterior, Uprev1, Uprev2, Uoutput] = calculateUpdates(Uinput, Uinterior, Uprev1, Uprev2, Uoutput, data.trainX(:,:,row), signals1, signals1prev, signals2, signals2prev, DjN, DiN, DpN);
+        for b=1:batch_size
+            if (i == size(data.trainX,3))
+               i = 1;
+            else 
+                i = i + 1;
+            end
+            [Ypred, signals1, signals1prev, signals2, signals2prev] = feedForward(data.trainX(:,:,i), Winput, Winterior, Wprev1, Wprev2, Woutput);
+            [DjN, DiN, DpN] = backpropagate(data.trainX(:,:,i), data.trainY(:,1,i), signals1 + signals1prev, signals2 + signals2prev, Ypred, Winterior, Wprev1, Wprev2, Woutput);       
+            [Uinput, Uinterior, Uprev1, Uprev2, Uoutput] = calculateUpdates(Uinput, Uinterior, Uprev1, Uprev2, Uoutput, data.trainX(:,:,i), signals1, signals1prev, signals2, signals2prev, DjN, DiN, DpN);
+            tmp_error = tmp_error + sum((Ypred(end,:) - data.trainY(end,1,i)).^2);
         end
         
-        %adaptive learning rate
+        train_error(iter) = tmp_error;
         tmp_error = inf;
-        while tmp_error > error(epoch, 1)
+        
+        %Adaptive learning rate
+        while (tmp_error > train_error(iter, 1)) && lambda > 10^-8
+            disp('adapt');
             tmp_error = 0;
-            tmp_Winput = Winput + lambda * Uinput/num_samples;
-            tmp_Winterior = Winterior + lambda * Uinterior/num_samples;
-            tmp_Wprev1 = Wprev1 + lambda * Uprev1/num_samples;
-            tmp_Wprev2 = Wprev2 + lambda * Uprev2/num_samples;
-            tmp_Woutput = Woutput + lambda * Uoutput/num_samples;
+            lambda = lambda/2;
+            tmp_Winput = Winput + lambda * Uinput/batch_size;
+            tmp_Winterior = Winterior + lambda * Uinterior/batch_size;
+            tmp_Wprev1 = Wprev1 + lambda * Uprev1/batch_size;
+            tmp_Wprev2 = Wprev2 + lambda * Uprev2/batch_size;
+            tmp_Woutput = Woutput + lambda * Uoutput/batch_size;
             
             %Forward pass through the network with validation data to see if error decreases
-	    for v=1:size(data.validateX, 3)
-            	[Ypred, signals1, signals1prev, signals2, signals2prev] = feedForward_struct(data.trainX(:, :, v), tmp_Winput, tmp_Winterior, tmp_Wprev1, tmp_Wprev2, tmp_Woutput);
-            	tmp_error = tmp_error + (data.trainY(:,:,v) - Ypred(end,:)).^2;
+            for b=1:batch_size
+                if (i == size(data.trainX,3))
+                   i = 1;
+                else 
+                    i = i + 1;
+                end
+                [Ypred, ~, ~, ~, ~] = feedForward(data.trainX(:,:,i), tmp_Winput, tmp_Winterior, tmp_Wprev1, tmp_Wprev2, tmp_Woutput);
+                tmp_error = tmp_error + sum((Ypred(end,:) - data.trainY(end,:,i)).^2);
             end
-	    tmp_error = tmp_error/size(data.validateX, 3);
-            lambda = lambda/2;
+            tmp_error = tmp_error/size(data.trainX, 3);
         end
         
         Winput = tmp_Winput;
@@ -47,10 +66,36 @@ function [Winput, Winterior, Wprev1, Wprev2, Woutput, error] = train_BP_adaptive
         Wprev2 = tmp_Wprev2;
         Woutput = tmp_Woutput;
         
-        error(epoch, 1) = tmp_error;
-	disp(tmp_error);
-        if (epoch < size(error, 1))
-            error(epoch+1, 1) = tmp_error;
+        %Calculate validation error
+        valid_error_tmp = 0;
+        for v=1:size(data.validateX,3)
+            [Ypred, ~, ~, ~, ~] = feedForward(data.validateX(:,:,v), Winput, Winterior, Wprev1, Wprev2, Woutput);
+            valid_error_tmp = valid_error_tmp + sum((Ypred(end,:) - data.validateY(end,:,v)).^2);
         end
+        valid_sum = valid_sum + valid_error_tmp;
+        valid_error_tmp = valid_error_tmp / size(data.validateX,3);
+        valid_error(iter, 1) = valid_sum/iter;
+        if (iter <= lookback + 1)
+            diff = 1000;
+        else
+            diff = abs(valid_error(iter-lookback,1) - valid_sum/iter);
+        end 
+        
+        if valid_error_tmp <= min_error
+            Winput_min = Winput;
+            Winterior_min = Winterior;
+            Wprev1_min = Wprev1;
+            Wprev2_min = Wprev2;
+            Woutput_min = Woutput;
+            min_error = valid_error_tmp;
+            disp(min_error);
+        end
+        iter = iter + 1;
     end
+    test_error = 0;
+    for i=1:size(data.testX,3)
+        [Ypred, ~, ~, ~, ~] = feedForward(data.testX(:,:,i), Winput_min, Winterior_min, Wprev1_min, Wprev2_min, Woutput_min);
+        test_error = test_error + sum((Ypred(end,:) - data.testY(end,:,i)).^2);
+    end
+    test_error = test_error/size(data.testX,3);
 end
